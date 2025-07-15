2b1b50d837e629cec2332ee209d04ffc3a5a34f7
USER <USER@DESKTOP-C408GBF>
2025-07-15 18:06:55 +0530

    Add project files.

---
+++ WebApplicationMVCLogin/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using WebApplicationMVCLogin.Models;
+using WebApplicationMVCLogin.Views.DBContect;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace WebApplicationMVCLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDBContext _ctx;
+
+        public AccountController(ApplicationDBContext db)
+        {
+            _ctx = db;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string command)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            if (command == "login")
+            {
+                string pwd=HashPassword(model.Password);
+                var user = _ctx.loginViewModels
+                .FirstOrDefault(u => u.Email == model.Email && u.Password == pwd); // Use hashing in prod
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Invalid email or password");
+                    return View(model);
+                }
+
+                var claims = new List<Claim>
+        {
+
+            new Claim(ClaimTypes.Email, user.Email ?? "")
+        };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity));
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            else if (command == "register")
+            {
+                model.Password = HashPassword(model.Password);
+                _ctx.loginViewModels.Add(model);
+                _ctx.SaveChanges();
+
+                TempData["Alert.Type"] = "success";       
+                TempData["Alert.Message"] = "You registered successfully!";
+
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+
+        public string HashPassword(string pwd)
+        {
+            using(var sha=SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(pwd);
+                var hash = sha.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ WebApplicationMVCLogin/Views/DBContect/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using WebApplicationMVCLogin.Models;
+
+namespace WebApplicationMVCLogin.Views.DBContect
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<LoginViewModel> loginViewModels { get; set; }
+
+    }
+}
